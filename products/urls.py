d7e71b948c0086be47c5d6ecb884f9be46687b7d
juanjosepenha4199 <j.penha@uniandes.edu.co>
2025-11-09 11:16:09 -0500

    si

--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-
-from . import views
-
-urlpatterns = [
-    path('productos/', views.variable_list, name='productosList'),
-    path('productocreate/', csrf_exempt(views.variable_create), name='productoCreate'),
-]
